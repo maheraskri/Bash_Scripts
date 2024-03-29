0bb8fbe40f39096c4bea1f165072a99ed863ab95
Meher Askri <meheraskri@gmx.com>
2024-03-29 07:03:07 +0100

    luks_script

--- luks.sh
+++ luks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 read -p "Please provide the path to your disk eg(/dev/vda,/dev/nvme0n1..): " disk
 read -p "please specify the size of the partition eg ( +2G..): " size
-# Creating a 1G partition 
+# Create a 1G partition 
 fdisk $disk <<EOF
 n
 p
@@ -14,25 +14,25 @@ EOF
 # Format partition with LUKS encryption
 echo "YES" | echo -n "testpass1234@" | cryptsetup luksFormat /dev/vdb1
 
-# Open the encrypted LUKS partition
+# Open Encrypted LUKS partition
 echo -n "testpass1234@" | cryptsetup luksOpen /dev/vdb1 new
 
-# Create an XFS filesystem
+# Create XFS filesystem
 mkfs.xfs /dev/mapper/new
 
-# Creating mount point and add it to /etc/fstab
+# Creating mount point && add it to /etc/fstab
 mkdir /test && echo -n "/dev/mapper/new		/test	xfs	defaults	0 0" >> /etc/fstab
 
 # Generating a random keyfile to useb for decryption
 dd if=/dev/urandom of=/root/keyfile bs=1024 count=4
 
-# Adding the KeyFile to the LUKS slots
+# Adding KeyFile to the LUKS slots
 echo -n "testpass1234@" | cryptsetup luksAddKey  /dev/vdb1 /root/keyfile
 
-# Adding the entry to /etc/crypttab
+# Adding entry to /etc/crypttab
 echo "new	/dev/vdb1	/root/keyfile"  >> /etc/crypttab
 
-# Removing the First Key 
+# Removing First Key 
 echo "YES" | echo -n "testpass1234@" | cryptsetup luksRemoveKey /dev/vdb1
 
 # Verifying that only one key left
@@ -42,4 +42,3 @@ if [ $? -eq 0 ]; then
 else
 	echo "Key Removel Failed" 
 fi
-

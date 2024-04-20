f08f7e42059bca73e757828c089fa47f89bb05ed
Meher Askri <meheraskri@gmx.com>
2024-04-20 20:20:04 +0100

    bash_script

--- backup.sh
+++ backup.sh
@@ -1,38 +1,33 @@
-#!/bin/bash 
-missing_option=false
-while getopts "s:d:c" meher ; do 
-	case $meher in 
-		s) source_dir=$OPTARG ;;
-		d) dest_dir=$OPTARG ;;
-		c) compress=true ;;
-		*) echo " Essayez de nouveau " 
-		   exit 1 ;;
-	esac 
+while getopts "s:d:c" meher ; do
+        case $meher in
+                s) source_dir=$OPTARG ;;
+                d) dest_dir=$OPTARG ;;
+                c) compress=true ;;
+                *) echo " Invalid or empty Option, Try Again " 
+                        exit 1 ;;
+        esac
 done
 shift $[ $OPTIND -1 ]
+
 if [ -z "$source_dir" ] || [ -z "$dest_dir" ]; then
-    missing_option=true
-    echo "Quelque chose manque, assurez-vous que -s et -d sont disponibles "
-    exit 1
+        echo " You missing something "
+        if [ -n "$compress" ] ; then
+        echo  " you can't use compress with nothing "
+        fi
+        exit 1
 fi
 
-if [ "$missing_option" = true ] && [ "$compress" = true ]; then
-    echo " L'option -c ne peut pas être utilisée sans les options -s et -d  "
-    exit 1
-fi
-
-basename="$(basename $source_dir)-$(date +%Y-%m-%d-%H)"
-
+basename="$(basename $source_dir)-$(date +%Y-%m-%d-%H)"                                             
 simple_function() {
 echo " perform backup $source_dir to $dest_dir .............." 
-if  [ "$compress" = true ] ; then 
-	echo "compress enabled"
-	tar -czvf "$dest_dir/$basename.tar.gz" $source_dir &>/dev/null
-	echo "backup terminée ..."
+if  [ "$compress" = true ] ; then
+        echo "compress enabled"
+        tar -czvf "$dest_dir/$basename.tar.gz" $source_dir &>/dev/null
+        echo "backup terminée ..."
 else
-	echo "compress disabled"
-	tar -cvf "$dest_dir/$basename.tar" $source_dir  &>/dev/null
-	echo "backup terminée ..."
+        echo "compress disabled"
+        tar -cvf "$dest_dir/$basename.tar" $source_dir  &>/dev/null
+        echo "backup terminée ..."
 fi
 }
 simple_function
